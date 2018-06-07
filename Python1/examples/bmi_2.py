a73a5cd1df83d49efbb6ca2cf01f6febd25ef274
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-07 08:15:08 +0200

    BMI index computation, 2nd version

---
+++ Python1/examples/bmi_2.py
@@ -0,0 +1,25 @@
+# Body Mass Index calculator
+
+import sys
+
+def compute_bmi(mass, height):
+    height = height / 100.0
+    bmi = mass / (height * height)
+    return bmi
+
+
+print("Mass (kg): ")
+mass = int(input())
+
+if mass < 0:
+    print("Invalid input")
+    sys.exit(1)
+
+print("Height (cm): ")
+height = int(input())
+
+if height < 0:
+    print("Invalid input")
+    sys.exit(1)
+
+print("BMI = ", compute_bmi(mass, height))
