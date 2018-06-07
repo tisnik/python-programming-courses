ce37f28502dff47c225cb0de2d82a5ad2b4540fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-07 14:08:36 +0200

    Simple loops

---
+++ Python1/examples/loops.py
@@ -0,0 +1,18 @@
+seznam = ["a", "b", "c"]
+
+for i in seznam:
+    print(i)
+
+print()
+
+ntice = (123, 456, 789)
+
+for i in ntice:
+    print(i)
+
+print()
+
+i = 1
+while i <= 2000:
+    print(i)
+    i *= 2
