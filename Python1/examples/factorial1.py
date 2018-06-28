61d41bbccfc0de35497e00c5a346bf8baf6a15f9
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-28 09:28:31 +0200

    Factorial, 1st version

---
+++ Python1/examples/factorial1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+
+def factorial(n):
+    """Výpočet faktoriálu ve smyčce."""
+    f = 1
+    for x in range(1, n + 1):
+        f *= x
+    return f
+
+
+# výpočet 10!
+print(factorial(10))
+
+# tabulka s hodnotami n!
+for n in range(1, 11):
+    print(n, factorial(n))
