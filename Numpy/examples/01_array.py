a545a6c9b4e35cefb48cb62a56b8581c2711781f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-24 13:59:13 +0200

    Numpy example 1: array constructor

---
+++ Numpy/examples/01_array.py
@@ -0,0 +1,11 @@
+# Knihovna Numpy
+#
+# konstrukce jednorozměrného pole konstruktorem numpy.array()
+
+import numpy
+
+# konstrukce pole
+a = numpy.array([1,2,3,4])
+
+# tisk obsahu pole na standardní výstup
+print(a)
