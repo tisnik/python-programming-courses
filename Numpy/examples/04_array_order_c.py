abd1c39556b5e10755cdc06eea2dcb9db2c0ee40
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-24 13:59:40 +0200

    Numpy example 4: array constructor, specifying C order

---
+++ Numpy/examples/04_array_order_c.py
@@ -0,0 +1,12 @@
+# Knihovna Numpy
+#
+# explicitní specifikace uspořádání prvků pole
+# (nemá velký význam pro 1D pole=vektory)
+
+import numpy
+
+# konstrukce pole
+a = numpy.array(range(10), order='C')
+
+# tisk obsahu pole na standardní výstup
+print(a)
