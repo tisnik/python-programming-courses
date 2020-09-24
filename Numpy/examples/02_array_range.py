bb1aacb29b6d5074c63d522fe478d0d195d67dbf
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-24 13:59:17 +0200

    Numpy example 2: array constructor

---
+++ Numpy/examples/02_array_range.py
@@ -0,0 +1,11 @@
+# Knihovna Numpy
+#
+# konstrukce jednorozměrného pole konstruktorem numpy.array()
+
+import numpy
+
+# konstrukce pole
+a = numpy.array(range(10))
+
+# tisk obsahu pole na standardní výstup
+print(a)
