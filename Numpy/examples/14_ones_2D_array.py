f6c3b697962393712e0395c52d7d80455f7537dd
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-25 08:31:00 +0200

    Example 14: ones() array constructor

---
+++ Numpy/examples/14_ones_2D_array.py
@@ -0,0 +1,14 @@
+# Knihovna Numpy
+#
+# Příklady použití konstruktoru numpy.ones
+#
+# matice se třemi řádky a čtyřmi sloupci
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.ones((3, 4))
+
+# tisk obsahu pole na standardní výstup
+print(a)
