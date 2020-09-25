3be672383eb518432ff26a5fc289909b93804f07
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-25 08:31:00 +0200

    Updated comments and formatting

--- Numpy/examples/01_array.py
+++ Numpy/examples/01_array.py
@@ -1,11 +1,14 @@
 # Knihovna Numpy
 #
-# konstrukce jednorozměrného pole konstruktorem numpy.array()
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
+# vytvoření pole se čtyřmi prvky
 
+# import hlavního balíčku knihovny Numpy
 import numpy
 
 # konstrukce pole
-a = numpy.array([1,2,3,4])
+a = numpy.array([1, 2, 3, 4])
 
 # tisk obsahu pole na standardní výstup
 print(a)
