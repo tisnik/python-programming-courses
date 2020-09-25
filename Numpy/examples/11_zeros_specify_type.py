abf39bb001d827afb961fd073c839031c998fff0
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-25 08:31:00 +0200

    Example 11: zeros() array constructor

---
+++ Numpy/examples/11_zeros_specify_type.py
@@ -0,0 +1,14 @@
+# Knihovna Numpy
+#
+# Příklady použití konstruktoru numpy.zeros
+#
+# matice o velikosti 5x5 prvků, každý prvek je typu int
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.zeros((5, 5), dtype=int)
+
+# tisk obsahu pole na standardní výstup
+print(a)
