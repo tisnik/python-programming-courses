dfca3395c64e1b5d277c5bfefb91b1719d77f24f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-25 08:31:00 +0200

    Updated comments and formatting

--- Numpy/examples/05_array_order_f.py
+++ Numpy/examples/05_array_order_f.py
@@ -1,8 +1,11 @@
 # Knihovna Numpy
 #
+# Konstrukce jednorozměrného pole konstruktorem numpy.array()
+#
 # explicitní specifikace uspořádání prvků pole
 # (nemá velký význam pro 1D pole=vektory)
 
+# import hlavního balíčku knihovny Numpy
 import numpy
 
 # konstrukce pole
