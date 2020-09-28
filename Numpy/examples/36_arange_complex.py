93a9e31990dcaea050f0897c910e5ecc8459cd98
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:17 +0200

    Example 36: arange constructor and complex numbers

---
+++ Numpy/examples/36_arange_complex.py
@@ -0,0 +1,15 @@
+# Knihovna Numpy
+#
+# Funkce numpy.arange
+#
+# nemusíme zůstat pouze u celých čísel, protože pracovat je možné i s hodnotami
+# typu float a complex
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(0+0j, 10+10j, 2+0j)
+
+# tisk obsahu pole na standardní výstup
+print(a)
