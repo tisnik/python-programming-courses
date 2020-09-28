845000535de0a8c42fa0da2dfb84051c43315d57
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:18 +0200

    Example 48: specifying item types

---
+++ Numpy/examples/48_geomspace_dtype.py
@@ -0,0 +1,14 @@
+# Knihovna Numpy
+#
+# Použití funkce numpy.geomspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít šest prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.geomspace(1, 100000, 6, dtype='I')
+
+# tisk obsahu pole na standardní výstup
+print(a)
