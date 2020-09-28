f1a4561d20c447866ee6f8a655ea0f70b976f3cf
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:17 +0200

    Example 33: using step definition in arrange array constructor

---
+++ Numpy/examples/33_arange_positive_step.py
@@ -0,0 +1,14 @@
+# Knihovna Numpy
+#
+# Funkce numpy.arange
+#
+# třetí nepovinný parametr určuje krok použitý při generování prvků vektoru
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(10, 20, 2)
+
+# tisk obsahu pole na standardní výstup
+print(a)
