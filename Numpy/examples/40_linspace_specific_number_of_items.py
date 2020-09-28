85316e92d908020f640c0c09ebf7240fb3993a29
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:17 +0200

    Example 40: linspace constructor with specific number of items

---
+++ Numpy/examples/40_linspace_specific_number_of_items.py
@@ -0,0 +1,15 @@
+# Knihovna Numpy
+#
+# Použití funkce numpy.linspace
+#
+# zde explicitně specifikujeme, že výsledný vektor má mít jedenáct prvků
+# (tím, že se začíná od nuly, získáme krok 0.1)
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.linspace(0, 1, 11)
+
+# tisk obsahu pole na standardní výstup
+print(a)
