c8413022d604e2d2f671f9b86ede6f59fa93eafa
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:14 +0200

    Example 26: 2D array constructed by full()

---
+++ Numpy/examples/26_full_2D_array.py
@@ -0,0 +1,14 @@
+# Knihovna Numpy
+#
+# Příklady použití konstruktoru numpy.full
+#
+# matice s rozměry 3x4 prvků
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.full((3, 4), 100)
+
+# tisk obsahu pole na standardní výstup
+print(a)
