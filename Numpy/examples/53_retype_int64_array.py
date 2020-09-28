21d1fce4e998cefd8c13cd0f3ee7a9c30833cf9c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-09-28 08:44:20 +0200

    Example 53: retype array items into int64

---
+++ Numpy/examples/53_retype_int64_array.py
@@ -0,0 +1,18 @@
+# Knihovna Numpy
+#
+# Přetypování
+#
+# konstrukce pole přetypováním seznamu
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# vytvoření běžného seznamu
+l = [1, 2, 3, 4]
+
+# přetypování (konstrukce pole daného typu)
+a = numpy.int64(l)
+
+# tisk typu a obsahu vytvořeného pole
+print(type(a))
+print(a)
