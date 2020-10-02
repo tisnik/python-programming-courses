df16a15ef7856375f5d9d02093aee071c6099475
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-02 08:31:00 +0200

    NumPy example #64: style of printing large array

---
+++ Numpy/examples/64_print_big_array.py
@@ -0,0 +1,12 @@
+# Knihovna Numpy
+#
+# Tisk velkých polí
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce velkého pole
+a = numpy.arange(10000).reshape(100, 100)
+
+# tisk velkého pole
+print(a)
