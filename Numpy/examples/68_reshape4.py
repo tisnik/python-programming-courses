b182ae418a7704ab1b9dfe40a166959fa92979ce
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-06 08:19:30 +0200

    Added missing import

--- Numpy/examples/68_reshape4.py
+++ Numpy/examples/68_reshape4.py
@@ -2,11 +2,14 @@
 #
 # Změna tvaru pole - vytvoření matice s jediným sloupcem
 
+# import hlavního balíčku knihovny Numpy
+import numpy
+
 # běžná matice se dvěma řádky a třemi sloupci
-a = array([[1, 2, 3], [4, 5, 6]])
+a = numpy.array([[1, 2, 3], [4, 5, 6]])
 
 # změna tvaru matice na jediný sloupec
-b = reshape(a, (6, 1))
+b = numpy.reshape(a, (6, 1))
 
 # tisk původní matice
 print(a)
