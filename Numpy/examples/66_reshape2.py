61da6f980dec3220e2c1eb6bc771e58bcb256eb6
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-06 08:19:25 +0200

    Added missing import

--- Numpy/examples/66_reshape2.py
+++ Numpy/examples/66_reshape2.py
@@ -3,11 +3,14 @@
 # Změna tvaru pole
 # zde vlastně dostaneme původní matici
 
+# import hlavního balíčku knihovny Numpy
+import numpy
+
 # běžná matice se dvěma řádky a třemi sloupci
-a = array([[1, 2, 3], [4, 5, 6]])
+a = numpy.array([[1, 2, 3], [4, 5, 6]])
 
 # změna tvaru matice na 3x2 prvky
-b = reshape(a, (2, 3))
+b = numpy.reshape(a, (2, 3))
 
 # tisk původní matice
 print(a)
