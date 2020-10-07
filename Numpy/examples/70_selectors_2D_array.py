37eca42fafd791ef5d8a849549cc08726146a833
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-07 08:21:23 +0200

    Numpy example #70: selector for 2D array

---
+++ Numpy/examples/70_selectors_2D_array.py
@@ -0,0 +1,18 @@
+# Knihovna Numpy
+#
+# ## Výběr prvků v poli
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# dvourozměrná pole - matice
+a = numpy.reshape(numpy.arange(12), (3, 4))
+
+# tisk původního pole
+print(a)
+
+# přístup k prvkům: řádek/sloupec
+print(a[0][2])
+
+# přístup k prvkům: řádek/sloupec
+print(a[2][0])
