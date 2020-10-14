52d3ae34c03ce6401d0fd5179509ae2846ec4858
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-14 08:06:21 +0200

    Numpy example 75: array slicing

---
+++ Numpy/examples/75_slicing.py
@@ -0,0 +1,18 @@
+# Knihovna Numpy
+#
+# Slicing - vynechání indexu/indexů
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# původní pole
+a = numpy.arange(12)
+
+# tisk původního pole
+print(a)
+
+# slicing
+b = a[3:7]
+
+# tisk nového pole
+print(b)
