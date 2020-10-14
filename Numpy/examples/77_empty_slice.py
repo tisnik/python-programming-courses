d79892015960800b25797a962f8884d10734a62b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-14 08:06:46 +0200

    Numpy example 77: empty slice

---
+++ Numpy/examples/77_empty_slice.py
@@ -0,0 +1,12 @@
+# Knihovna Numpy
+#
+# Prázdný řez polem
+
+# import hlavního balíčku knihovny Numpy
+import numpy
+
+# konstrukce pole
+a = numpy.arange(12)
+
+# provedení řezu polem
+print(a[-4:-6])
