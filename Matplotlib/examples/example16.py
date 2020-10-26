6e19f45c4f90f53425ac147e85da8ce9ee6c9833
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-26 08:05:36 +0100

    New histogram parameter naming conventions

--- Matplotlib/examples/example16.py
+++ Matplotlib/examples/example16.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 # náhodné hodnoty
 y = np.random.normal(0, 0.1, 10000)
 
-plt.hist(y, bins=30, range=None, normed=True)
+plt.hist(y, bins=30, range=None, density=True)
 
 # zobrazení grafu
 plt.show()
