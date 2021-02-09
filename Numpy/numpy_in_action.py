78e71856452b2bcb7e356026fe84e091f04c883f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-02-09 10:19:19 +0100

    Fixed issue #210

--- Numpy/numpy_in_action.py
+++ Numpy/numpy_in_action.py
@@ -1451,7 +1451,7 @@ print(a[a % 2 == 0])
 # ## Další užitečné funkce - min, max, sum
 
 # původní pole
-a1=array([[1, 2, 3], [4,5,6], [7,8,9]])
+a1=array([[1, 2, 3], [4, 5,6], [7,8,9]])
 
 # tisk výsledku operace
 print(a1.max())
