077299d226f8b6e7d8f55a3b8d994adfcd4ec424
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-02 08:44:55 +0100

    Fixed issue #242

--- Numpy/numpy_in_action.py
+++ Numpy/numpy_in_action.py
@@ -1506,7 +1506,7 @@ print(b)
 # - Opět se specifikuje osa
 
 # původní pole
-a1 = array([[1, 2, 3], [4,5,6], [7,8,9]])
+a1 = array([[1, 2, 3], [4, 5,6], [7,8,9]])
 
 # aplikace anonymní funkce
 a = apply_along_axis(lambda v: v[1], 0, a1)
