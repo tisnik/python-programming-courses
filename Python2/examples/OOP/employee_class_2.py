b2724fe7e671d801fea262a9f351899548e583d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-12 10:42:27 +0100

    Employee class #2

---
+++ Python2/examples/OOP/employee_class_2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+
+class Employee:
+    """Třída reprezentující zaměstnance."""
+
+    def __init__(self, first_name, surname, salary):
+        """Konstruktor objektu."""
+        self._first_name = first_name
+        self._surname = surname
+        self._salary = salary
+
+    def display_employee(self):
+        """Metoda pro výpis hodnoty objektu."""
+        print("Full name: {name} {surname}   Salary: {salary}".format(
+            name=self._first_name,
+            surname=self._surname,
+            salary=self._salary))
+
+
+# vytvoření dvou instancí třídy
+employee1 = Employee("Eda", "Wasserfall", 10000)
+employee2 = Employee("Přemysl", "Hájek", 25001)
+
+# výpis hodnot objektů
+employee1.display_employee()
+employee2.display_employee()
