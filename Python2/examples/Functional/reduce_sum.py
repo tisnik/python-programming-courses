fe72306c345e09d26d73e6e85a6d301104043328
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-15 07:38:18 +0100

    Reduce function

---
+++ Python2/examples/Functional/reduce_sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Funkce vyššího řádu reduce."""
+
+from functools import reduce
+
+x = range(1, 101)
+
+print(x)
+
+y = reduce(lambda a, b: a+b, x)
+print(y)
