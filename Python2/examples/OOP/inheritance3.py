79fbafd554f99ef6d0c3921ae3a9f0c5e4777310
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-15 07:38:10 +0100

    Interitance example #3

---
+++ Python2/examples/OOP/inheritance3.py
@@ -0,0 +1,36 @@
+class Person:
+
+    def __init__(self, first_name, surname):
+        """Konstruktor objektu."""
+        self._first_name = first_name
+        self._surname = surname
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "**Person** Full name: {name} {surname}".format(
+                name=self._first_name,
+                surname=self._surname)
+
+
+class Student(Person):
+    def __init__(self, first_name, surname):
+        """Konstruktor objektu."""
+        super().__init__(first_name, surname)
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "**Student** Full name: {name} {surname}".format(
+                name=self._first_name,
+                surname=self._surname)
+
+
+# vytvoření dvou instancí třídy
+p1 = Person("Eda", "Wasserfall")
+p2 = Person("Přemysl", "Hájek")
+
+print(p1)
+print(p2)
+
+s1 = Student("John", "Doe")
+
+print(s1)
