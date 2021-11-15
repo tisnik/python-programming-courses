271b18565d26895803fa1645800f5c2dcc0a7fe3
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-15 07:38:10 +0100

    Polymorphism example #2

---
+++ Python2/examples/OOP/polymorphism2.py
@@ -0,0 +1,57 @@
+class Printable:
+    def display(self):
+        print(self)
+
+
+class Person(Printable):
+
+    def __init__(self, first_name, surname):
+        """Konstruktor objektu."""
+        print("Person.__init__")
+        self._first_name = first_name
+        self._surname = surname
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "**Person** Full name: {name} {surname}".format(
+                name=self._first_name,
+                surname=self._surname)
+
+
+class Student(Person):
+    def __init__(self, first_name, surname):
+        """Konstruktor objektu."""
+        print("Student.__init__")
+        super().__init__(first_name, surname)
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "**Student** Full name: {name} {surname}".format(
+                name=self._first_name,
+                surname=self._surname)
+
+
+class Employee(Person):
+    def __init__(self, first_name, surname, salary):
+        """Konstruktor objektu."""
+        print("Employee.__init__")
+        super().__init__(first_name, surname)
+        self._salary = salary
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "**Employee** Full name: {name} {surname}   Salary: {salary}".format(
+                name=self._first_name,
+                surname=self._surname,
+                salary=self._salary)
+
+
+people = [
+    Person("Eda", "Wasserfall"),
+    Person("Přemysl", "Hájek"),
+    Student("John", "Doe"),
+    Employee("Eric", "Iverson", 10000)
+]
+
+for p in people:
+    p.display()
