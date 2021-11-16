2791895e5d8abbaf099236a947722d0dce2ef188
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-16 08:16:41 +0100

    Employee class, 1st version

---
+++ Python2/examples/OOP/employee_class_1.py
@@ -0,0 +1,14 @@
+"""Kostra jednoduché třídy reprezentující zaměstnance."""
+
+
+class Employee:
+    """Kostra třídy reprezentující zaměstnance."""
+    pass
+
+# vytvoření dvou instancí třídy
+employee1 = Employee()
+employee2 = Employee()
+
+# výpis hodnot objektů
+print(employee1)
+print(employee2)
