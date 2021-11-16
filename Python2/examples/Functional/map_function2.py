e2a58ffcec01d0b24b11b0cb6060fa21f6ecf2b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-16 08:28:51 +0100

    Map function, second example

---
+++ Python2/examples/Functional/map_function2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Funkce vyššího řádu map."""
+
+def inc(x):
+    return x+1
+
+x = range(10)
+
+print(x)
+
+y = map(inc, x)
+print(list(y))
