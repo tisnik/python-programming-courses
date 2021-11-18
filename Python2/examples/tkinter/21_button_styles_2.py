82cc5e4b18e979491a90f0bf2848c2bb4d6e461c
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-18 18:45:39 +0100

    Example 21: button styles 2

---
+++ Python2/examples/tkinter/21_button_styles_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+
+def exit():
+    sys.exit(0)
+
+
+root = tkinter.Tk()
+
+style = ttk.Style()
+style.configure('Red.TButton', background='#ff8080')
+
+buttonStyles = ("sunken", "solid", "flat", "groove", "raised", "ridge")
+
+buttons = (tkinter.Button(root, text=buttonStyle, relief=buttonStyle)
+           for buttonStyle in buttonStyles)
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+for i, button in enumerate(buttons):
+    button.grid(column=1, row=i, sticky="we")
+
+quitButton.grid(column=2, row=6, sticky="we")
+
+root.mainloop()
