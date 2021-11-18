40f51ad31527368ea27aa2ecbc31fa5d32bb39e9
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-18 18:45:39 +0100

    Example 06: buttons and pack manager

---
+++ Python2/examples/tkinter/06_buttons_and_pack_manager.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+from tkinter import *
+from tkinter import ttk
+
+import sys
+
+root = Tk()
+
+button1 = ttk.Button(root, text="First button")
+button2 = ttk.Button(root, text="Second button with long text")
+button3 = ttk.Button(root, text="Third button")
+button4 = ttk.Button(root, text="Fourth button")
+
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+
+root.mainloop()
