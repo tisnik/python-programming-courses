eaa048b029a3e94be6a99fafa03e19489a25928f
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-18 18:45:39 +0100

    Example 05: buttons in regular grid cmd

---
+++ Python2/examples/tkinter/05_buttons_in_regular_grid_cmd.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+button1 = ttk.Button(root, text="First button", command=lambda: sys.exit(0))
+button2 = ttk.Button(root, text="Second button", command=lambda: sys.exit(0))
+button3 = ttk.Button(root, text="Third button", command=lambda: sys.exit(0))
+button4 = ttk.Button(root, text="Fourth button", command=lambda: sys.exit(0))
+
+button1.grid(column=1, row=1)
+button2.grid(column=2, row=1)
+button3.grid(column=1, row=2)
+button4.grid(column=2, row=2)
+
+root.mainloop()
