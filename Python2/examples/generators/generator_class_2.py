09e7d65b095aa22202082543812e7750636ed4c1
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-18 10:13:14 +0100

    Generator class

---
+++ Python2/examples/generators/generator_class_2.py
@@ -0,0 +1,18 @@
+class infinite_generator():
+
+    def __init__(self):
+        self.n = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        current, self.n = self.n, self.n+1
+        return current
+
+
+for i in infinite_generator():
+    print(i)
+    if i >= 10:
+        break
+
