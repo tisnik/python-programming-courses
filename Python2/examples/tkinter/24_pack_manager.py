60f686a77662135a51029d630dfa54ebfdaa64ac
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-18 18:45:39 +0100

    Example 24: pack manager

---
+++ Python2/examples/tkinter/24_pack_manager.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+
+def exit():
+    sys.exit(0)
+
+
+root = tkinter.Tk()
+
+style = ttk.Style()
+style.configure('Red.TButton', background='#ff8080')
+
+button1 = ttk.Button(root, text="1s button", command=exit)
+button2 = ttk.Button(root, text="2nd button with long text", command=exit)
+button3 = ttk.Button(root, text="3rd button", command=exit)
+button4 = ttk.Button(root, text="4th button", command=exit)
+
+quitButton = ttk.Button(root, text="Exit", style='Red.TButton',
+                        command=exit)
+
+button1.pack()
+button2.pack()
+button3.pack()
+button4.pack()
+
+label = tkinter.Label(root, text='Hello world')
+entry = tkinter.Entry(root)
+checkbutton = tkinter.Checkbutton(text='Do you like Tkinter?')
+
+checkbutton.pack()
+label.pack()
+entry.pack()
+
+quitButton.pack()
+
+root.mainloop()
