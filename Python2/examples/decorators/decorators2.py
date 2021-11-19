f4d06fe30955518d36234505c016db6e4892f5a1
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-19 15:38:50 +0100

    Decorators, 2nd step

---
+++ Python2/examples/decorators/decorators2.py
@@ -0,0 +1,16 @@
+def wrapper1(function):
+
+    def inner_function():
+        print("-"*40)
+        function()
+        print("-"*40)
+
+    return inner_function
+
+
+@wrapper1
+def hello():
+    print("Hello!")
+
+
+hello()
