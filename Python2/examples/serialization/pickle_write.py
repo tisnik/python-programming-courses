fd27d782d1480daad57362fcebb606ff9605a846
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-22 08:24:51 +0100

    Serializing value

---
+++ Python2/examples/serialization/pickle_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Serializce hodnot do souboru."""
+
+import pickle
+
+data = {
+        "x": "Hello",
+        "y": "world",
+        "z": [1, 2, 3, 4],
+        "w": (1, 2, 3, 4)}
+
+with open("test", "wb") as fout:
+    pickle.dump(data, fout)
