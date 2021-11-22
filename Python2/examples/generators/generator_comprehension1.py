849f46674e186841183a6c80952f5ad9dbcf667b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-22 07:48:11 +0100

    Generator comprehension

---
+++ Python2/examples/generators/generator_comprehension1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+seznam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+g1 = (item for item in seznam)
+
+g2 = (item*2 for item in seznam)
+
+g3 = (item for item in seznam if item % 3 == 0)
+
+print(seznam)
+print(g1)
+print(g2)
+print(g3)
