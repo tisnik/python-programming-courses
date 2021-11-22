588e0f4d61e44ab4c8f125ee556902916d4749cc
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-22 07:48:11 +0100

    Updated comments

--- Python2/examples/OOP/inheritance4.py
+++ Python2/examples/OOP/inheritance4.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Volání konstruktoru nadtřídy, rozlišení konstruktorů."""
+
+
 class Person:
 
     def __init__(self, first_name, surname):
