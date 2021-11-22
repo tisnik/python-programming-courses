66732ecc98063b5359aaeea5b43e0491f2fb0c03
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-22 07:48:10 +0100

    New example

--- Python2/examples/Functional/list_comprehension.py
+++ Python2/examples/Functional/list_comprehension.py
@@ -1,9 +1,14 @@
+"""Generátorová notace seznamu."""
+
 seznam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+seznam1 = [item for item in seznam]
 
 seznam2 = [item*2 for item in seznam]
 
 seznam3 = [item for item in seznam if item % 3 == 0]
 
 print(seznam)
+print(seznam1)
 print(seznam2)
 print(seznam3)
