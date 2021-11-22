4ff0a3c4d8a49f8207f310baaba9f7aeae03a40a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-22 08:24:51 +0100

    Using shelve to deserialize objects

---
+++ Python2/examples/serialization/employee_class_shelve_read.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+"""Přečtení objektů z databáze."""
+
+import shelve
+
+
+class Employee:
+    """Třída reprezentující zaměstnance."""
+
+    def __init__(self, first_name, surname, salary):
+        """Konstruktor objektu."""
+        self._first_name = first_name
+        self._surname = surname
+        self._salary = salary
+        self._poznamka = 124
+
+    def __str__(self):
+        """Speciální metoda pro převod objektu na řetězec."""
+        return "name: {name} {surname}   Salary: {salary}".format(name=self._first_name,
+                                                                  surname=self._surname,
+                                                                  salary=self._salary)
+
+    def __eq__(self, other):
+        if other is None:
+            return False
+        return self._first_name == other._first_name and \
+            self._surname == other._surname and \
+            self._salary == other._salary
+
+
+with shelve.open("test") as db:
+    print(db["employee1"])
+    print(db["employee2"])
+    print(db["employee3"])
