30d571ab70fe13f8d0a5b3e0f6180a1e4bfb9311
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-23 08:35:46 +0100

    Environment

---
+++ Python2/examples/stdlib/environ.py
@@ -0,0 +1,7 @@
+import os
+
+def get_event_level():
+    """Get level of events to monitor (errors only, or error and warnings)."""
+    if os.environ.get("SENTRY_CATCH_WARNINGS", False):
+        return logging.WARNING
+    return logging.ERROR
