ba09f2083f86682cc1b747d228f757037f9f31a5
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 21:24:37 +0100

    Example 37: listbox

---
+++ Python2/examples/tkinter/37_listbox.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+langs = ("Assembler", "Basic", "Brainfuck", "C", "Python")
+
+listbox = tkinter.Listbox(root)
+
+
+for lang in langs:
+    listbox.insert(tkinter.END, lang)
+
+
+quitButton = ttk.Button(root, text="Exit", command=exit)
+
+listbox.pack()
+quitButton.pack()
+
+root.mainloop()
