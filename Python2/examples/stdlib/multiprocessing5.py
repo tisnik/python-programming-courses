1f5942401eac11e2a01d9bcaaf56310a4c47fe2b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 08:27:23 +0100

    Multiprocessing, example #5

---
+++ Python2/examples/stdlib/multiprocessing5.py
@@ -0,0 +1,27 @@
+from multiprocessing import Process, Pipe
+import time
+
+
+def worker(name, conn):
+    while True:
+        cmd = conn.recv()
+        print("{} received {}".format(name, cmd))
+        if cmd == "quit":
+            return
+        else:
+            conn.send("{} accepted {}".format(name, cmd))
+        time.sleep(1)
+
+
+parent_conn, child_conn = Pipe()
+
+p = Process(target=worker, args=("Worker", child_conn))
+p.start()
+
+for i in range(10):
+    parent_conn.send("command {}".format(i))
+    print(parent_conn.recv())
+
+parent_conn.send("quit")
+
+p.join()
