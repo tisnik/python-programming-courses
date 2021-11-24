2d983dd96b9b178f8f8160b48232f295238390bd
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 08:27:17 +0100

    Multiprocessing, example #3

---
+++ Python2/examples/stdlib/multiprocessing3.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+import time
+
+
+def worker(name):
+    print("hello", name)
+    time.sleep(5)
+    print("done", name)
+
+
+ps = [Process(target=worker, args=(name,)) for name in ("foo", "bar", "baz", "other")]
+
+for p in ps:
+    p.start()
+
+for p in ps:
+    p.join()
