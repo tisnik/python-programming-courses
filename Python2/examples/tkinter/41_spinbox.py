2f04dada712db662a0c369a9bd904c4022c00a3d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 21:24:37 +0100

    Example 41: spinbox

---
+++ Python2/examples/tkinter/41_spinbox.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+import sys
+
+root = tkinter.Tk()
+
+number = tkinter.IntVar()
+
+spinbox = tkinter.Spinbox(root, from_=100, to=120, width=10,
+                          textvariable=number)
+
+showButton = ttk.Button(root, text="Show var",
+                        command=lambda: print(number.get()))
+
+quitButton = ttk.Button(root, text="Exit", command=exit)
+
+spinbox.grid(column=1, row=1)
+showButton.grid(column=1, row=2)
+quitButton.grid(column=2, row=2)
+
+root.mainloop()
