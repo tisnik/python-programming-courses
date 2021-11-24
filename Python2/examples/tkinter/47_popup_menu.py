b9b2f0997b524a8a6984ddd263f3abb72303d142
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 21:24:37 +0100

    Example 47: popup menu

---
+++ Python2/examples/tkinter/47_popup_menu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+import tkinter
+from tkinter import ttk
+
+
+def test():
+    print("Test!")
+
+
+root = tkinter.Tk()
+
+popup = tkinter.Menu(root, tearoff=0)
+
+popup.add_command(label="Open")
+popup.add_command(label="Save")
+popup.add_separator()
+popup.add_command(label="Exit", command=root.quit)
+
+
+def on_popup(event):
+    popup.post(event.x_root-5, event.y_root-5)
+
+
+root.bind("<Button-3>", on_popup)
+root.mainloop()
