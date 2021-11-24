f74644e51ff066ea4400271790ff6ef05cfd2bb7
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-11-24 14:12:25 +0100

    Compatibility with Python 3.9

--- Python2/examples/stdlib/multiprocessing3.py
+++ Python2/examples/stdlib/multiprocessing3.py
@@ -8,10 +8,16 @@ def worker(name):
     print("done", name)
 
 
-ps = [Process(target=worker, args=(name,)) for name in ("foo", "bar", "baz", "other")]
+def main():
+    ps = [Process(target=worker, args=(name,)) for name in ("foo", "bar", "baz", "other")]
 
-for p in ps:
-    p.start()
+    for p in ps:
+        p.start()
 
-for p in ps:
-    p.join()
+    for p in ps:
+        p.join()
+
+
+if __name__ == '__main__':
+    print("Running main")
+    main()
