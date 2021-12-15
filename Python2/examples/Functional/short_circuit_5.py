9024ee0fefab9a6d47e2406b53a39c76db86a0df
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-15 09:38:08 +0100

    Reformatted by black

--- Python2/examples/Functional/short_circuit_5.py
+++ Python2/examples/Functional/short_circuit_5.py
@@ -3,13 +3,16 @@
 
 """Zkrácené vyhodnocení logických operátorů."""
 
+
 def x():
     print("x() called")
     return False
 
+
 def y():
     print("y() called")
     return False
+
 
 if x() and y():
     print("if branch")
