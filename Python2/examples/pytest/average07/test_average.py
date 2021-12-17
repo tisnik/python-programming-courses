bb1bfb929dad94091f7e1d9e159cfeb30fe054b0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:37:42 +0100

    Formatted by black

--- Python2/examples/pytest/average07/test_average.py
+++ Python2/examples/pytest/average07/test_average.py
@@ -6,11 +6,11 @@ from average import average
 
 
 testdata = [
-        ((1, 1), 1),
-        ((1, 2), 1.5),
-        ((0, 1), 0.5),
-        ((1, 2, 3), 2.0),
-        ((0, 10), 0.5),
+    ((1, 1), 1),
+    ((1, 2), 1.5),
+    ((0, 1), 0.5),
+    ((1, 2, 3), 2.0),
+    ((0, 10), 0.5),
 ]
 
 
@@ -18,7 +18,9 @@ testdata = [
 def test_average_basic(values, expected):
     """Otestování výpočtu průměru."""
     result = average(values)
-    assert result == expected, "Očekávaná hodnota {}, vráceno {}".format(expected, result)
+    assert result == expected, "Očekávaná hodnota {}, vráceno {}".format(
+        expected, result
+    )
 
 
 def test_average_empty_list_1():
