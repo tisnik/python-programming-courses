f648834ab57a6eab4fdc3239874e7bbae1d0d60a
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:37:42 +0100

    Formatted by black

--- Python2/examples/pytest/lifecycle_module/test_module.py
+++ Python2/examples/pytest/lifecycle_module/test_module.py
@@ -24,10 +24,10 @@ def test_2(printer):
 
 
 testdata = [
-        (0, 1),
-        (1, 2),
-        (2, 3),
-        (3, 4),
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 4),
 ]
 
 
@@ -35,5 +35,7 @@ testdata = [
 def test_succ(printer, value, expected):
     """Otestování výpočtu následují hodnoty v číselné řadě."""
     printer("Test succ")
-    result = value+1
-    assert result == expected, "Očekávaná hodnota {}, vráceno {}".format(expected, result)
+    result = value + 1
+    assert result == expected, "Očekávaná hodnota {}, vráceno {}".format(
+        expected, result
+    )
