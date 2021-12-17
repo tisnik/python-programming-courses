340a3f4d6db8f3a11c23ee2a08a131d244ff1402
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:34:48 +0100

    Formatted by black

--- Python2/examples/OOP/polymorphism3.py
+++ Python2/examples/OOP/polymorphism3.py
@@ -1,5 +1,4 @@
 class Person:
-
     def __init__(self, first_name, surname):
         """Konstruktor objektu."""
         print("Person.__init__")
@@ -12,8 +11,8 @@ class Person:
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "**Person** Full name: {name} {surname}".format(
-                name=self._first_name,
-                surname=self._surname)
+            name=self._first_name, surname=self._surname
+        )
 
 
 class Student(Person):
@@ -28,8 +27,8 @@ class Student(Person):
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "**Student** Full name: {name} {surname}".format(
-                name=self._first_name,
-                surname=self._surname)
+            name=self._first_name, surname=self._surname
+        )
 
 
 class Employee(Person):
@@ -45,16 +44,15 @@ class Employee(Person):
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "**Employee** Full name: {name} {surname}   Salary: {salary}".format(
-                name=self._first_name,
-                surname=self._surname,
-                salary=self._salary)
+            name=self._first_name, surname=self._surname, salary=self._salary
+        )
 
 
 people = [
     Person("Eda", "Wasserfall"),
     Person("Přemysl", "Hájek"),
     Student("John", "Doe"),
-    Employee("Eric", "Iverson", 10000)
+    Employee("Eric", "Iverson", 10000),
 ]
 
 for p in people:
