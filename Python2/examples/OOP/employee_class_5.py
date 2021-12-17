237de60b1e1c018e3848f6586860682eeb3f8786
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:34:48 +0100

    Formatted by black

--- Python2/examples/OOP/employee_class_5.py
+++ Python2/examples/OOP/employee_class_5.py
@@ -16,9 +16,8 @@ class Employee:
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "Full name: {name} {surname}   Salary: {salary}".format(
-                name=self._first_name,
-                surname=self._surname,
-                salary=self._salary)
+            name=self._first_name, surname=self._surname, salary=self._salary
+        )
 
 
 # vytvoření dvou instancí třídy
