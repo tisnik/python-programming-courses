56f1215a1cdb2079594c1c0e1f046e9da59f2f6d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:37:42 +0100

    Formatted by black

--- Python2/examples/pytest/average14/test_code_style.py
+++ Python2/examples/pytest/average14/test_code_style.py
@@ -11,7 +11,9 @@ from average import average
 def test_code_style():
     files = list(Path(".").rglob("*.py"))
 
-    style = pycodestyle.StyleGuide(quiet=False, config_file='setup.cfg')
+    style = pycodestyle.StyleGuide(quiet=False, config_file="setup.cfg")
     result = style.check_files(files)
     print("Total errors:", result.total_errors)
-    assert result.total_errors == 0, "Detected {} code style problems".format(result.total_errors)
+    assert result.total_errors == 0, "Detected {} code style problems".format(
+        result.total_errors
+    )
