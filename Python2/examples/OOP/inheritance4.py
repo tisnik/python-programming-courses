4a671234b53cd58177ae56c3ae5ddf24e670bb7d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-12-17 09:34:48 +0100

    Formatted by black

--- Python2/examples/OOP/inheritance4.py
+++ Python2/examples/OOP/inheritance4.py
@@ -5,7 +5,6 @@
 
 
 class Person:
-
     def __init__(self, first_name, surname):
         """Konstruktor objektu."""
         print("Person.__init__")
@@ -15,8 +14,8 @@ class Person:
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "**Person** Full name: {name} {surname}".format(
-                name=self._first_name,
-                surname=self._surname)
+            name=self._first_name, surname=self._surname
+        )
 
 
 class Student(Person):
@@ -28,8 +27,8 @@ class Student(Person):
     def __str__(self):
         """Speciální metoda pro převod objektu na řetězec."""
         return "**Student** Full name: {name} {surname}".format(
-                name=self._first_name,
-                surname=self._surname)
+            name=self._first_name, surname=self._surname
+        )
 
 
 # vytvoření dvou instancí třídy
