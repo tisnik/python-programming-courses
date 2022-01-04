ea613fa3e8feea0141708b477ede1f4b39069cda
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-04 10:53:15 +0100

    Code formatted by black

--- Python1/examples/socket_server.py
+++ Python1/examples/socket_server.py
@@ -2,7 +2,7 @@ import socket
 
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-connection.bind(('0.0.0.0', 5555))
+connection.bind(("0.0.0.0", 5555))
 connection.listen(10)
 
 current_connection, address = connection.accept()
