9cbd1260458835aaf74e00d22962c36b4096ebe6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-04 10:53:15 +0100

    Code formatted by black

--- Python1/examples/socket_client.py
+++ Python1/examples/socket_client.py
@@ -3,7 +3,7 @@ import socket
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-connection.connect(('127.0.0.1', 5555))
+connection.connect(("127.0.0.1", 5555))
 
 message = b"Hello world\r\n"
 
