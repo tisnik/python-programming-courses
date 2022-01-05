8509fc3bf63567d104f681b1e5e25a94c0f68cff
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-05 09:23:56 +0100

    Fixed style with black

--- Python2/examples/mocking/mock-test7/test.py
+++ Python2/examples/mocking/mock-test7/test.py
@@ -11,35 +11,35 @@ def test1():
     print("function1 returns: {v}".format(v=value))
 
 
-@patch('module2.function2', return_value="*mocked*")
+@patch("module2.function2", return_value="*mocked*")
 def test2(mocked_function):
     print("*** test2 ***")
     value = function1()
     print("function1 returns: {v}".format(v=value))
 
 
-@patch('module1.function2', return_value="*mocked*")
+@patch("module1.function2", return_value="*mocked*")
 def test3(mocked_function):
     print("*** test3 ***")
     value = function1()
     print("function1 returns: {v}".format(v=value))
 
 
-@patch('module2.function3', return_value="*mocked*")
+@patch("module2.function3", return_value="*mocked*")
 def test4(mocked_function):
     print("*** test4 ***")
     value = function1()
     print("function1 returns: {v}".format(v=value))
 
 
-@patch('module3.function3', return_value="*mocked*")
+@patch("module3.function3", return_value="*mocked*")
 def test5(mocked_function):
     print("*** test5 ***")
     value = function1()
     print("function1 returns: {v}".format(v=value))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test1()
     print()
 
