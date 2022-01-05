2b1f043dc7dc508fc958422307409a61954be15b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-05 09:23:56 +0100

    Fixed style with black

--- Python2/examples/mocking/mock-test1/test.py
+++ Python2/examples/mocking/mock-test1/test.py
@@ -10,13 +10,13 @@ def test1():
     print(application.function1())
 
 
-@patch('application.function1', return_value=42)
+@patch("application.function1", return_value=42)
 def test2(mocked_function):
     """Druhý test používá fake test double - náhradu volané funkce."""
     print(application.function1())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test1()
     print()
 
