71e918732a1b01f082ca9b18e50081bb12243b11
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-07 13:10:38 +0100

    Reformatted by black

--- Python2/examples/decorators/measure_time.py
+++ Python2/examples/decorators/measure_time.py
@@ -6,11 +6,10 @@ import time
 
 
 def measure_time(func):
-
     def wrapper(*arg):
         t = time.time()
         res = func(*arg)
-        print("Function took " + str(time.time()-t) + " seconds to run")
+        print("Function took " + str(time.time() - t) + " seconds to run")
         return res
 
     return wrapper
