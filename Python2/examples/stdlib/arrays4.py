d74e48ca848a8f68f4b58b0edadfc907c4f7aaa9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-10 09:34:38 +0100

    Reformatted by black

--- Python2/examples/stdlib/arrays4.py
+++ Python2/examples/stdlib/arrays4.py
@@ -4,10 +4,10 @@
 
 from array import array
 
-a1 = array('l')
-a2 = array('u')
-a3 = array('l')
-a4 = array('d')
+a1 = array("l")
+a2 = array("u")
+a3 = array("l")
+a4 = array("d")
 
 
 with open("a1", "rb") as fin:
@@ -27,7 +27,7 @@ print(a2)
 print(a3)
 print(a4)
 
-print(a1.buffer_info()[1]*a1.itemsize)
-print(a2.buffer_info()[1]*a2.itemsize)
-print(a3.buffer_info()[1]*a3.itemsize)
-print(a4.buffer_info()[1]*a4.itemsize)
+print(a1.buffer_info()[1] * a1.itemsize)
+print(a2.buffer_info()[1] * a2.itemsize)
+print(a3.buffer_info()[1] * a3.itemsize)
+print(a4.buffer_info()[1] * a4.itemsize)
