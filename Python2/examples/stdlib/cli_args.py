adf7320a3f943085cacc0d829c52b5cc1faa3c75
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-10 09:34:38 +0100

    Reformatted by black

--- Python2/examples/stdlib/cli_args.py
+++ Python2/examples/stdlib/cli_args.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 
+
 def cli_arguments():
     """Retrieve all CLI arguments provided by user."""
     # First of all, we need to specify all command line flags that are
@@ -9,28 +10,67 @@ def cli_arguments():
     parser = ArgumentParser()
 
     # All supported command line arguments and flags
-    parser.add_argument("-a", "--address", dest="address", required=False,
-                        help="Address of REST API for external data pipeline")
+    parser.add_argument(
+        "-a",
+        "--address",
+        dest="address",
+        required=False,
+        help="Address of REST API for external data pipeline",
+    )
 
-    parser.add_argument("-u", "--user", dest="user", required=False,
-                        help="User name for basic authentication")
+    parser.add_argument(
+        "-u",
+        "--user",
+        dest="user",
+        required=False,
+        help="User name for basic authentication",
+    )
 
-    parser.add_argument("-p", "--password", dest="password", required=False,
-                        help="Password for basic authentication")
+    parser.add_argument(
+        "-p",
+        "--password",
+        dest="password",
+        required=False,
+        help="Password for basic authentication",
+    )
 
-    parser.add_argument("-i", "--input", dest="input", default=None, required=False,
-                        help="Specification of input file (with list of clusters, for example)")
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="input",
+        default=None,
+        required=False,
+        help="Specification of input file (with list of clusters, for example)",
+    )
 
-    parser.add_argument("-c", "--compare-results", dest="compare_results", action="store_true",
-                        default=None, required=False,
-                        help="Compare two sets of results, each set stored in its own directory")
+    parser.add_argument(
+        "-c",
+        "--compare-results",
+        dest="compare_results",
+        action="store_true",
+        default=None,
+        required=False,
+        help="Compare two sets of results, each set stored in its own directory",
+    )
 
-    parser.add_argument("-e", "--export", dest="export_file_name", required=False,
-                        default="report.csv",
-                        help="Name of CSV file with exported comparison results")
+    parser.add_argument(
+        "-e",
+        "--export",
+        dest="export_file_name",
+        required=False,
+        default="report.csv",
+        help="Name of CSV file with exported comparison results",
+    )
 
-    parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=None,
-                        help="Make messages verbose", required=False)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="verbose",
+        action="store_true",
+        default=None,
+        help="Make messages verbose",
+        required=False,
+    )
 
     # Now it is time to parse flags, check the actual content of command line
     # and fill-in the object named `args`.
