1362c41ddda915f9faab2b5793bf8b69d3311e99
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-10 09:34:39 +0100

    Reformatted by black

--- Python2/examples/stdlib/thread_pool_1.py
+++ Python2/examples/stdlib/thread_pool_1.py
@@ -3,10 +3,9 @@ import time
 
 
 def worker(threadName, delay, n):
-    for counter in range(1, n+1):
+    for counter in range(1, n + 1):
         time.sleep(delay)
         print("{}: {}/{} - {}".format(threadName, counter, n, time.ctime(time.time())))
-
 
 
 with ThreadPoolExecutor(max_workers=3) as executor:
