7f2d224904fce3609019d7815e5694c79d6ef8b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-10 09:34:38 +0100

    Reformatted by black

--- Python2/examples/stdlib/bounds.py
+++ Python2/examples/stdlib/bounds.py
@@ -1,8 +1,11 @@
 class Bounds:
-
-    def __init__(self,
-                 xmin=sys.float_info.max, ymin=sys.float_info.max,
-                 xmax=-sys.float_info.max, ymax=-sys.float_info.max):
+    def __init__(
+        self,
+        xmin=sys.float_info.max,
+        ymin=sys.float_info.max,
+        xmax=-sys.float_info.max,
+        ymax=-sys.float_info.max,
+    ):
         """Construct new bounds using given coordinates or default values."""
         self.xmin = xmin
         self.ymin = ymin
