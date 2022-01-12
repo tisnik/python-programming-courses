b5baf9439cf25c0e7b9246dfa7e2fb6926897030
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-12 09:38:34 +0100

    Reformatted by black

--- Python2/examples/generators/generator_comprehension2.py
+++ Python2/examples/generators/generator_comprehension2.py
@@ -5,7 +5,7 @@ seznam = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 g1 = list(item for item in seznam)
 
-g2 = list(item*2 for item in seznam)
+g2 = list(item * 2 for item in seznam)
 
 g3 = list(item for item in seznam if item % 3 == 0)
 
