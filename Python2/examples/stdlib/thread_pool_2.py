272bcd0e572a66621141bd8947972df67f687934
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-12 09:38:35 +0100

    Reformatted by black

--- Python2/examples/stdlib/thread_pool_2.py
+++ Python2/examples/stdlib/thread_pool_2.py
@@ -3,18 +3,17 @@ import time
 
 
 def worker(threadName, delay, n):
-    for counter in range(1, n+1):
+    for counter in range(1, n + 1):
         time.sleep(delay)
         print("{}: {}/{} - {}".format(threadName, counter, n, time.ctime(time.time())))
     print("{}: DONE!".format(threadName))
-
 
 
 workers = 10
 
 with ThreadPoolExecutor(max_workers=workers) as executor:
     for w in range(workers):
-        executor.submit(worker, "Thread-{}".format(w+1), 0.5 + w/10.0, 10)
+        executor.submit(worker, "Thread-{}".format(w + 1), 0.5 + w / 10.0, 10)
 
 
 print("Done!")
