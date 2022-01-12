9a41128ea1e73fa7fd390b96760056abeaaab320
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-12 09:38:34 +0100

    Reformatted by black

--- Python2/examples/mocking/mock-testB/main.py
+++ Python2/examples/mocking/mock-testB/main.py
@@ -4,7 +4,7 @@
 from module1 import *
 
 
-if __name__ == '__main__':
-    print(add(1,0))
-    print(add(1,1))
-    print(add(1,2))
+if __name__ == "__main__":
+    print(add(1, 0))
+    print(add(1, 1))
+    print(add(1, 2))
