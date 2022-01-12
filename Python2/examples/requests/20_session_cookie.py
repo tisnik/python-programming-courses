e2ca9259195b8ea7879ec5d6b5c497526232dc6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-12 09:38:34 +0100

    Reformatted by black

--- Python2/examples/requests/20_session_cookie.py
+++ Python2/examples/requests/20_session_cookie.py
@@ -7,10 +7,12 @@ import json
 
 def set_cookie(session, name, value):
     # adresa s testovaci REST API sluzbou
-    URL = "https://httpbin.org/cookies/set/{name}/{value}".format(name=name, value=value)
+    URL = "https://httpbin.org/cookies/set/{name}/{value}".format(
+        name=name, value=value
+    )
 
     # hlavicka posilana v dotazu
-    headers = {'accept': 'application/json'}
+    headers = {"accept": "application/json"}
 
     # poslani HTTP dotazu typu GET
     return session.get(URL, headers=headers)
@@ -21,7 +23,7 @@ def delete_cookie(session, name):
     URL = "https://httpbin.org/cookies/delete?{name}=".format(name=name)
 
     # hlavicka posilana v dotazu
-    headers = {'accept': 'application/json'}
+    headers = {"accept": "application/json"}
 
     # poslani HTTP dotazu typu GET
     return session.get(URL, headers=headers)
