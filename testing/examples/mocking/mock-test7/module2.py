1706478d23c5d874f4b9dacd6535b1fcbcc45bc9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-15 08:24:03 +0100

    Mocking in Python, example #7

---
+++ testing/examples/mocking/mock-test7/module2.py
@@ -0,0 +1,6 @@
+from module3 import *
+
+
+def function2():
+    print("called: function2")
+    return "function2 " + function3()
---
+++ testing/examples/mocking/mock-test7/module3.py
@@ -0,0 +1,3 @@
+def function3():
+    print("called: function3")
+    return "function3"
