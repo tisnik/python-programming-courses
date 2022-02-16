485e81e9071e03c3d36814215c71ac649532d813
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-16 19:18:08 +0100

    Fixed formatting

--- Python2/examples/xml/sax2.py
+++ Python2/examples/xml/sax2.py
@@ -4,7 +4,7 @@ import xml.sax
 class XmlHandler(xml.sax.ContentHandler):
     def startElement(self, name, attributes):
         print("Node:", name)
-        for (k,v) in attributes.items():
+        for (k, v) in attributes.items():
             print("\t", k, v)
 
 
