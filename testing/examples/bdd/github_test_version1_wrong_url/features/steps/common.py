2fe7db55a7e43bf8068491f761a2b49115ba7ad6
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/github_test_version1_wrong_url/features/steps/common.py
+++ testing/examples/bdd/github_test_version1_wrong_url/features/steps/common.py
@@ -5,17 +5,17 @@ from urllib.parse import urljoin
 import requests
 
 
-@given('GitHub is accessible')
+@given("GitHub is accessible")
 def initial_state(context):
     assert context.is_accessible(context)
 
 
-@when('I access the API endpoint {url}')
+@when("I access the API endpoint {url}")
 def access_endpoint(context, url):
     context.response = requests.get(context.api_url + url)
 
 
-@then('I should receive {status:d} status code')
+@then("I should receive {status:d} status code")
 def check_status_code(context, status):
     """Check the HTTP status code returned by the REST API."""
     assert context.response.status_code == status
