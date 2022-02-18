8934d22acb811a8f2af165e3e32932543058099b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:55:24 +0100

    Formatted by black

--- testing/examples/requests/13_cookies_tests.py
+++ testing/examples/requests/13_cookies_tests.py
@@ -53,12 +53,10 @@ def test_cookies():
     URL = "http://httpbin.org/cookies"
 
     # hlavicka posilana v dotazu
-    headers = {'accept': 'application/json'}
+    headers = {"accept": "application/json"}
 
     # priprava cookies
-    cookies = {'key1': 'value1',
-               'key2': 'value2',
-               'key3': 'value3'}
+    cookies = {"key1": "value1", "key2": "value2", "key3": "value3"}
 
     # poslani HTTP dotazu typu GET
     response = requests.get(URL, headers=headers, cookies=cookies)
