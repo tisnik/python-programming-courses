5da7609803283b1c29b2ea81a2f9597430bf88f3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/test_python_function2/features/steps/common.py
+++ testing/examples/bdd/test_python_function2/features/steps/common.py
@@ -17,19 +17,20 @@ from behave import given, then, when
 from src.adder import add
 
 
-@given('The function {function_name} is callable')
+@given("The function {function_name} is callable")
 def initial_state(context, function_name):
     g = globals()
     assert function_name in g, "Function is not visible"
     assert callable(g[function_name]), "Not a function"
 
 
-@when('I call function {function} with arguments {x:d} and {y:d}')
+@when("I call function {function} with arguments {x:d} and {y:d}")
 def call_add(context, function, x, y):
     context.result = add(x, y)
 
 
-@then('I should get {expected:d} as a result')
+@then("I should get {expected:d} as a result")
 def check_integer_result(context, expected):
-    assert context.result == expected, \
-        "Wrong result: {r} != {e}".format(r=context.result, e=expected)
+    assert context.result == expected, "Wrong result: {r} != {e}".format(
+        r=context.result, e=expected
+    )
