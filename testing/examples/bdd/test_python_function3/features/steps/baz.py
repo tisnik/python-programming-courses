5087b2198f699a86481842479e02b815b5435a70
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/test_python_function3/features/steps/baz.py
+++ testing/examples/bdd/test_python_function3/features/steps/baz.py
@@ -2,7 +2,8 @@ from behave import given, then, when
 from src.adder import add
 
 
-@then('I should get {expected:d} as a result')
+@then("I should get {expected:d} as a result")
 def check_integer_result(context, expected):
-    assert context.result == expected, \
-        "Wrong result: {r} != {e}".format(r=context.result, e=expected)
+    assert context.result == expected, "Wrong result: {r} != {e}".format(
+        r=context.result, e=expected
+    )
