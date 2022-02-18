e491656e0aefa2bf46d89946fbad16a61fbce77f
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/multiline_text2/features/steps/common.py
+++ testing/examples/bdd/multiline_text2/features/steps/common.py
@@ -4,20 +4,21 @@ from behave import given, then, when
 from src.to_uppercase import to_uppercase
 
 
-@given(u'a sample text')
+@given(u"a sample text")
 def a_sample_text(context):
     assert context.text
     context.input = context.text
 
 
-@when(u'I translate the text to upper case')
+@when(u"I translate the text to upper case")
 def step_impl(context):
     """Zavolani testovane funkce."""
     context.result = to_uppercase(context.input)
 
 
-@then('I should get the following text as a result')
+@then("I should get the following text as a result")
 def check_result(context):
     """Porovnani vypocteneho vysledku s vysledkem ocekavanym."""
-    assert context.result == context.text, \
-        "Wrong result: {r} != {e}".format(r=context.result, e=context.text)
+    assert context.result == context.text, "Wrong result: {r} != {e}".format(
+        r=context.result, e=context.text
+    )
