910bb3f94edf29e9178597d63429bfc477ee07c7
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/test_python_function3/features/steps/foo.py
+++ testing/examples/bdd/test_python_function3/features/steps/foo.py
@@ -2,6 +2,6 @@ from behave import given, then, when
 from src.adder import add
 
 
-@given('The function {function_name} is callable')
+@given("The function {function_name} is callable")
 def initial_state(context, function_name):
     pass
