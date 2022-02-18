f380e2c94b5baff7ab3108ffc101595570f8710e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/exchange_rate1_invalid_calc/features/steps/common.py
+++ testing/examples/bdd/exchange_rate1_invalid_calc/features/steps/common.py
@@ -1,7 +1,7 @@
 from behave import given, then, when
 
 
-@given(u'the following exchange rate table')
+@given(u"the following exchange rate table")
 def exchange_rate_table_init(context):
     # slovnik do ktereho se ulozi smenne kurzy
     tbl = {}
@@ -18,20 +18,21 @@ def exchange_rate_table_init(context):
     context.exchange_rate_table = tbl
 
 
-@when(u'I sell {sold} {currency}')
+@when(u"I sell {sold} {currency}")
 def step_impl(context, sold, currency):
     """Vypocet na zaklade smenneho kurzu."""
     exchange_rate = context.exchange_rate_table[currency]
     context.result = exchange_rate * float(sold) * 2
 
 
-@then(u'I should receive {amount:g} CZK')
+@then(u"I should receive {amount:g} CZK")
 def step_impl(context, amount):
     """Porovnani vypocteneho vysledku s vysledkem ocekavanym."""
-    assert isclose(context.result, amount, rel_tol=1e-5), \
-        "Wrong result: {r} != {a}".format(r=context.result, a=amount)
+    assert isclose(
+        context.result, amount, rel_tol=1e-5
+    ), "Wrong result: {r} != {a}".format(r=context.result, a=amount)
 
 
 def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
     """Pomocna funkce pro porovnani dvou cisel s plovouci radovou carkou."""
-    return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+    return abs(a - b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
