30416271fb3c15d2a3bfb726a30105b9022ccda8
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/github_test_version4/features/steps/common.py
+++ testing/examples/bdd/github_test_version4/features/steps/common.py
@@ -5,60 +5,60 @@ from urllib.parse import urljoin
 import requests
 
 
-@given('GitHub is accessible')
+@given("GitHub is accessible")
 def initial_state(context):
     assert context.is_accessible(context)
 
 
-@given('System is running')
+@given("System is running")
 def running_system(context):
     """Ensure that the system is accessible."""
     assert is_accessible(context)
 
 
-@when('I access the API endpoint {url}')
+@when("I access the API endpoint {url}")
 def access_endpoint(context, url):
     context.response = requests.get(context.api_url + url)
 
 
-@when('I search for user with nick {nick}')
+@when("I search for user with nick {nick}")
 def search_for_user(context, nick):
     url = urljoin(urljoin(context.api_url, "users/"), nick)
     context.response = requests.get(url)
 
 
-@then('I should receive {status:d} status code')
+@then("I should receive {status:d} status code")
 def check_status_code(context, status):
     """Check the HTTP status code returned by the REST API."""
     assert context.response.status_code == status
 
 
-@then('I should receive proper JSON response')
+@then("I should receive proper JSON response")
 def check_json_response(context):
-    content_type = context.response.headers.get('content-type')
-    assert content_type.startswith('application/json')
+    content_type = context.response.headers.get("content-type")
+    assert content_type.startswith("application/json")
     context.data = context.response.json()
 
 
-@then('I should find the user with full name {fullname}')
+@then("I should find the user with full name {fullname}")
 def check_user_full_name(context, fullname):
     assert context.data is not None
-    assert 'name' in context.data
-    value = context.data.get('name')
+    assert "name" in context.data
+    value = context.data.get("name")
     assert value == fullname, "{e} != {v}".format(e=fullname, v=value)
 
 
-@then('I should find that the user works for company {company}')
+@then("I should find that the user works for company {company}")
 def check_company(context, company):
     assert context.data is not None
-    assert 'company' in context.data
-    value = context.data.get('company', '')
+    assert "company" in context.data
+    value = context.data.get("company", "")
     assert value == company, "{e} != {v}".format(e=company, v=value)
 
 
-@then('I should find that the user works for company ')
+@then("I should find that the user works for company ")
 def check_company_not_set(context):
     assert context.data is not None
-    assert 'company' in context.data
-    value = context.data.get('company', '')
+    assert "company" in context.data
+    value = context.data.get("company", "")
     assert not value
