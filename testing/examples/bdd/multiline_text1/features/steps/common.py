9d465f1d78b138bfa05f64d3584a41845870a4f2
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-18 16:58:17 +0100

    Formatted by black

--- testing/examples/bdd/multiline_text1/features/steps/common.py
+++ testing/examples/bdd/multiline_text1/features/steps/common.py
@@ -4,20 +4,21 @@ from behave import given, then, when
 from src.count_words import count_words
 
 
-@given(u'a sample text')
+@given(u"a sample text")
 def a_sample_text(context):
     assert context.text
     context.input = context.text
 
 
-@when(u'I count all words in text')
+@when(u"I count all words in text")
 def step_impl(context):
     """Zavolani testovane funkce."""
     context.result = count_words(context.input)
 
 
-@then('I should get {expected:d} as a result')
+@then("I should get {expected:d} as a result")
 def check_result(context, expected):
     """Porovnani vypocteneho vysledku s vysledkem ocekavanym."""
-    assert context.result == expected, \
-        "Wrong result: {r} != {e}".format(r=context.result, e=expected)
+    assert context.result == expected, "Wrong result: {r} != {e}".format(
+        r=context.result, e=expected
+    )
