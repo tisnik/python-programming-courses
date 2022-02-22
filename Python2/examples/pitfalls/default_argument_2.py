b79e7f0339c6ddb0c11c661f56295327a7b8d6a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-22 08:59:20 +0100

    Reformatted by black

--- Python2/examples/pitfalls/default_argument_2.py
+++ Python2/examples/pitfalls/default_argument_2.py
@@ -1,11 +1,13 @@
 # výchozí hodnota funkce je vyhodnocena pouze jedenkrát
 # a to ve chvíli definici funkce
 
+
 def foo(bar=None):
     if bar is None:
         bar = []
     bar.append("baz")
     return bar
+
 
 print(foo())
 
